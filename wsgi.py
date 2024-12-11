a235e5b5594065d1741b57b0a35a12822a62f1e0
StergiosCha <kafouroutsos@hotmail.com>
2024-12-11 04:51:21 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, Blueprint, request, jsonify, render_template
+from app.routes.text_routes import text_bp
 
 app = Flask(__name__)
 
+# Register the blueprint
+app.register_blueprint(text_bp, url_prefix='/api/text')
+
 @app.route('/')
 def home():
-    return "NATS is running!"
+    return render_template('upload.html')
