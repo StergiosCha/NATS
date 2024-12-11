b9514b605d9d47008a37cd4963295adcf7976adf
StergiosCha <kafouroutsos@hotmail.com>
2024-12-11 05:06:51 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,8 +9,4 @@ app.register_blueprint(text_bp, url_prefix='/api/text')
 
 @app.route('/')
 def home():
-    return render_template('upload.html')
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    return "NATS is running!"
