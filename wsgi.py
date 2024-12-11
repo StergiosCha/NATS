e94834c03abf1f00a82b2f1b1cc4142568e47ca4
StergiosCha <kafouroutsos@hotmail.com>
2024-12-11 09:55:29 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import os
 import spacy
+from app.models.doc_embeddings import DocEmbeddingsAnalyzer
 
 app = Flask(__name__, template_folder='app/templates')
 nlp = spacy.load('el_core_news_md')
@@ -17,9 +18,19 @@ def upload_files():
     files = request.files.getlist('files')
     texts = {}
     
+    # Load and process texts
     for file in files:
         if file.filename:
             text = file.read().decode('utf-8')
-            texts[file.filename] = text
+            # Process with spaCy
+            doc = nlp(text)
+            texts[file.filename] = doc
             
-    return render_template('results.html', files=texts)
+    # Perform document embeddings analysis
+    doc_analyzer = DocEmbeddingsAnalyzer()
+    doc_embeddings = doc_analyzer.create_embeddings(texts)
+    doc_viz = doc_analyzer.visualize_embeddings('both')
+            
+    return render_template('results.html', 
+                         files=texts, 
+                         doc_viz=doc_viz)
