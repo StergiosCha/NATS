34154091b0d0975421baf8fd3a644c3c8446eb88
StergiosCha <kafouroutsos@hotmail.com>
2024-12-11 05:13:26 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,6 @@
-from flask import Flask, Blueprint, request, jsonify, render_template
-from app.routes.text_routes import text_bp
-import os
+from flask import Flask
 
 app = Flask(__name__)
-
-# Register the blueprint
-app.register_blueprint(text_bp, url_prefix='/api/text')
 
 @app.route('/')
 def home():
