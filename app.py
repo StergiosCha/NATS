76371dc34449592226c649daa65cec1c1526e52e
StergiosCha <kafouroutsos@hotmail.com>
2024-12-11 04:28:12 +0100

    Update app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from app.routes.text_routes import text_bp
+
+def create_app():
+    app = Flask(__name__)
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+    
+    # Register blueprints
+    app.register_blueprint(text_bp, url_prefix='/api/text')
+    
+    return app
