d5ce2a20d7bdd4d4c676841f8e6fe361b93be695
StergiosCha <kafouroutsos@hotmail.com>
2024-12-11 05:01:19 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, request, jsonify, render_template
 from app.routes.text_routes import text_bp
+import os
 
 app = Flask(__name__)
 
@@ -9,3 +10,7 @@ app.register_blueprint(text_bp, url_prefix='/api/text')
 @app.route('/')
 def home():
     return render_template('upload.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
