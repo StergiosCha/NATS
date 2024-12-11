dc542322c26522a42a4dfa8b75c8b356891736bc
StergiosCha <kafouroutsos@hotmail.com>
2024-12-11 04:43:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,7 @@
 from flask import Flask
-from app.routes.text_routes import text_bp
 
-def create_app():
-    app = Flask(__name__)
-    app.config['UPLOAD_FOLDER'] = 'uploads'
-    
-    # Register blueprints
-    app.register_blueprint(text_bp, url_prefix='/api/text')
-    
-    return app
+app = Flask(__name__)
 
-app = create_app()
+@app.route('/')
+def home():
+    return "NATS is running!"
